4141577200f601ac9b2a551c240b12a3b948cea0
Yaryna Serdiukova <karpenko_yaryna@ukr.net>
2022-12-28 17:48:28 +0300

    Add title lista_zakupow.py

---
+++ lista_zakupow.py
@@ -0,0 +1,20 @@
+print("Список покупок")
+x=0
+
+zakupy_dict = {
+"пекрня": ["хліб", "булочки", "пончик"],
+"овочевий": ["морква", "селера", "рукола"]
+}
+
+
+for k,v in zakupy_dict.items():
+          
+     print (f"Я йду до {k} і купую там {v}")
+     
+     x+=len(v)
+
+#jj = sum([len(v) for v in zakupy_dict.values()])
+#print(jj)
+  
+print(f"Разом купую {x} товарів")
+ 
---
+++ zadanie_diapazon.py
@@ -0,0 +1,7 @@
+
+diapazon_list  = [number for number in range(1,101) if not number % 5]
+print(f" Всі числа від 0 до 100 які діляться на 5 {diapazon_list} ")
+
+squares = [number **3 for number in diapazon_list]
+print(f"А це ті числа піднесені до 3 степеня {squares}")
+
